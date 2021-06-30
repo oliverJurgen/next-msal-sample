1869b98ed8b98fe0a83b0e3a9ee5fcd7632caccd
oliverJurgen <oliver.jurgen90@gmail.com>
2021-06-30 13:05:43 +0800

    change deployment redirect URI

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,23 +1,24 @@
 // Config object to be passed to Msal on creation
 export const msalConfig = {
-    auth: {
-        clientId: "5e937ff8-5858-408a-879a-c1b215e783f9",
-        authority: "https://login.microsoftonline.com/common",
-        redirectUri: "/",
-        postLogoutRedirectUri: "/",
-
-        // redirectUri: "http://localhost:3000/",
-        // postLogoutRedirectUri: "http://localhost:3000/"
-    },
+  auth: {
+    clientId: "5e937ff8-5858-408a-879a-c1b215e783f9",
+    authority: "https://login.microsoftonline.com/common",
+    redirectUri: "https://next-msal-sample.vercel.app/",
+    postLogoutRedirectUri: "https://next-msal-sample.vercel.app/",
+    // redirectUri: "/",
+    // postLogoutRedirectUri: "/",
+    // redirectUri: "http://localhost:3000/",
+    // postLogoutRedirectUri: "http://localhost:3000/"
+  },
 };
 
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const loginRequest = {
-    scopes: ["User.Read"],
+  scopes: ["User.Read"],
 };
 
 // Add here the endpoints for MS Graph API services you would like to use.
 export const graphConfig = {
-    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
-    // graphMeEndpoint: "https://graph.microsoft-ppe.com/v1.0/me",
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+  // graphMeEndpoint: "https://graph.microsoft-ppe.com/v1.0/me",
 };
