13740ba4674c47ca9e8acaa64c50d1643650a830
oliverJurgen <oliver.jurgen90@gmail.com>
2021-07-01 08:52:35 +0800

    retrieve access token

--- src/MsGraphApiCall.js
+++ src/MsGraphApiCall.js
@@ -2,27 +2,32 @@ import { loginRequest, graphConfig } from "./authConfig";
 import { msalInstance } from "../pages/_app";
 
 export async function callMsGraph() {
-    const account = msalInstance.getActiveAccount();
-    if (!account) {
-        throw Error("No active account! Verify a user has been signed in and setActiveAccount has been called.");
-    }
+  const account = msalInstance.getActiveAccount();
+  if (!account) {
+    throw Error(
+      "No active account! Verify a user has been signed in and setActiveAccount has been called."
+    );
+  }
 
-    const response = await msalInstance.acquireTokenSilent({
-        ...loginRequest,
-        account: account
-    });
+  const response = await msalInstance.acquireTokenSilent({
+    ...loginRequest,
+    account: account,
+  });
 
-    const headers = new Headers();
-    const bearer = `Bearer ${response.accessToken}`;
+  console.log({ acquireTokenResponse: response });
+  const headers = new Headers();
+  const bearer = `Bearer ${response.accessToken}`;
 
-    headers.append("Authorization", bearer);
+  headers.append("Authorization", bearer);
 
-    const options = {
-        method: "GET",
-        headers: headers
-    };
+  const options = {
+    method: "GET",
+    headers: headers,
+  };
 
-    return fetch(graphConfig.graphMeEndpoint, options)
-        .then(response => response.json())
-        .catch(error => console.log(error));
+  console.log(`API CALL now to our server with ${JSON.stringify(options)} `);
+
+  //   return fetch(graphConfig.graphMeEndpoint, options)
+  //     .then((response) => response.json())
+  //     .catch((error) => console.log(error));
 }
--- src/authConfig.js
+++ src/authConfig.js
@@ -1,7 +1,8 @@
+const clientId = "5e937ff8-5858-408a-879a-c1b215e783f9";
 // Config object to be passed to Msal on creation
 export const msalConfig = {
   auth: {
-    clientId: "5e937ff8-5858-408a-879a-c1b215e783f9",
+    clientId,
     authority:
       "https://dantegulapa.b2clogin.com/dantegulapa.onmicrosoft.com/B2C_1_signupsignin1",
     knownAuthorities: ["dantegulapa.b2clogin.com"],
@@ -43,10 +44,11 @@ export const msalConfig = {
   // },
 };
 
+// **Added clientId in scopes to recieve accessToken see: https://github.com/AzureAD/microsoft-authentication-library-for-js/issues/2315
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const loginRequest = {
   // scopes: ["User.Read"],
-  scopes: ["openid", "offline_access"],
+  scopes: ["openid", "offline_access", clientId],
 };
 
 const tokenRequest = {
